f2a4fd28de7238eccbe0ecdeba35fc178df20900
abid7955 <syedmohd.abid.21cse@bmu.edu.in>
2024-01-23 12:28:37 +0530

    Added the Gallery Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Cards from "./components/CardsMT/CardsMT";
+import Gallery from "./components/Gallery/Gallery";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <About />
       <Cards/>
+      <Gallery/>
     </>
   );
 }
