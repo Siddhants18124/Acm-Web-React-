f11150e9e27f44a5dc25a5b6b4669452d48e70bb
Prakhar Kumar Srivastava <prakharkumar.srivastava.22cse@bmu.edu.in>
2024-01-26 00:23:35 +0530

    team&gallery-content

--- src/assets/assets.jsx
+++ src/assets/assets.jsx
@@ -15,7 +15,43 @@ import logoTwo from "../assets/images/logoTwo.png";
 import Gallery1 from "../assets/images/Gallery1.png";
 import Prakhar from "../assets/images/Team/Prakhar.png";
 import Dakshi from "../assets/images/Team/Dakshi.jpeg";
-import Pic from "../assets/images/Gallery/pic.jpeg";
+import Aditya from "../assets/images/Team/Aditya.jpg";
+import Abid from "../assets/images/Team/Abid.jpg";
+import AKJ from "../assets/images/Team/AKJ.jpg";
+import Ananya from "../assets/images/Team/Ananya.jpg";
+import Anmol from "../assets/images/Team/Anmol.jpg";
+import Daksh from "../assets/images/Team/Daksh.jpg";
+import Raghv from "../assets/images/Team/Raghv.jpg";
+import Shashank from "../assets/images/Team/Shashank.png";
+import Siddhant from "../assets/images/Team/Siddhant.jpg";
+import Vedansh from "../assets/images/Team/Vedansh.jpg";
+import garv from "../assets/images/Team/garv.jpg";
+import Yashvardhan from "../assets/images/Team/Yashvardhan.jpg";
+import Guneet from "../assets/images/Team/Guneet.jpg";
+import pic5 from "../assets/images/Gallery/pic5.jpg";
+import pic6 from "../assets/images/Gallery/pic6.jpeg";
+import pic7 from "../assets/images/Gallery/pic7.jpg";
+import pic8 from "../assets/images/Gallery/pic8.jpeg";
+import pic9 from "../assets/images/Gallery/pic9.jpg";
+import pic10 from "../assets/images/Gallery/pic10.jpeg";
+import pic11 from "../assets/images/Gallery/pic11.jpg";
+import pic12 from "../assets/images/Gallery/pic12.jpeg";
+import pic13 from "../assets/images/Gallery/pic13.jpg";
+import pic14 from "../assets/images/Gallery/pic14.jpeg";
+import pic15 from "../assets/images/Gallery/pic15.jpg";
+import pic16 from "../assets/images/Gallery/pic16.jpeg";
+import pic17 from "../assets/images/Gallery/pic17.jpg";
+import pic18 from "../assets/images/Gallery/pic18.jpeg";
+import pic19 from "../assets/images/Gallery/pic19.jpg";
+import pic20 from "../assets/images/Gallery/pic20.jpeg";
+import pic21 from "../assets/images/Gallery/pic21.jpg";
+import pic22 from "../assets/images/Gallery/pic22.jpg";
+import pic23 from "../assets/images/Gallery/pic23.jpeg";
+import pic24 from "../assets/images/Gallery/pic24.jpeg";
+import pic25 from "../assets/images/Gallery/pic25.jpg";
+import pic26 from "../assets/images/Gallery/pic26.jpg";
+import pic27 from "../assets/images/Gallery/pic27.jpg";
+import pic28 from "../assets/images/Gallery/pic28.jpg";
 
 const assets = {
   Logo,
@@ -26,9 +62,32 @@ const assets = {
   Pic2,
   Pic3,
   Pic4,
+  pic5,
+  pic6,
+  pic7,
+  pic8,
+  pic9,
+  pic10,
+  pic11,
+  pic12,
+  pic13,
+  pic14,
+  pic15,
+  pic16,
+  pic17,
+  pic18,
+  pic19,
+  pic20,
+  pic21,
+  pic22,
+  pic23,
+  pic24,
+  pic25,
+  pic26,
+  pic27,
+  pic28,
   sphereTopTwo,
   sphereBottomTwo,
-  Pic,
   vectorTwo,
   vectorThree,
   BMULOGO,
@@ -36,6 +95,19 @@ const assets = {
   Gallery1,
   Prakhar,
   Dakshi,
+  Abid,
+  Aditya,
+  AKJ,
+  Ananya,
+  Anmol,
+  Daksh,
+  Raghv,
+  Shashank,
+  Siddhant,
+  Vedansh,
+  Yashvardhan,
+  garv,
+  Guneet,
 };
 
 export default assets;
--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -24,116 +24,116 @@ function Gallery() {
         <div className="grid">
           <div className="image-grid">
             <div className="image-container">
-              <img srcSet={assets.Pic1} />
-              <div className="tooltip">Image 1</div>
+              <img srcSet={assets.pic5} />
+              <div className="tooltip">WEB 3</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.pic25} />
+              <div className="tooltip">DEVOPS</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.pic26} />
+              <div className="tooltip">WORKSHOP</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.pic27} />
+              <div className="tooltip">OPEN SOURCE</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.pic28} />
+              <div className="tooltip">WORKSHOP</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.pic18} />
+              <div className="tooltip">SIH 2023</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.pic19} />
+              <div className="tooltip">FLUTTER</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.pic20} />
+              <div className="tooltip">WORKSHOP</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.pic21} />
+              <div className="tooltip">WORKSHOP</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.Pic4} />
+              <div className="tooltip">WORKSHOP</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.pic23} />
+              <div className="tooltip">WORKSHOP</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.pic24} />
+              <div className="tooltip">WORKSHOP</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.pic22} />
+              <div className="tooltip">WORKSHOP</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.Pic3} />
+              <div className="tooltip">WORKSHOP</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.pic11} />
+              <div className="tooltip">WORKSHOP</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.pic12} />
+              <div className="tooltip">WORKSHOP</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.pic13} />
+              <div className="tooltip">WORKSHOP</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.pic14} />
+              <div className="tooltip">WORKSHOP</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.pic15} />
+              <div className="tooltip">WORKSHOP</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.pic16} />
+              <div className="tooltip">WORKSHOP</div>
+            </div>
+            <div className="image-container">
+              <img srcSet={assets.pic17} />
+              <div className="tooltip">WORKSHOP</div>
             </div>
             <div className="image-container">
               <img srcSet={assets.Pic2} />
-              <div className="tooltip">Image 2</div>
+              <div className="tooltip">WORKSHOP</div>
             </div>
             <div className="image-container">
-              <img srcSet={assets.Pic1} />
-              <div className="tooltip">Image 1</div>
+              <img srcSet={assets.pic6} />
+              <div className="tooltip">WORKSHOP</div>
             </div>
             <div className="image-container">
-              <img srcSet={assets.Pic2} />
-              <div className="tooltip">Image 2</div>
+              <img srcSet={assets.pic7} />
+              <div className="tooltip">WORKSHOP</div>
             </div>
             <div className="image-container">
-              <img srcSet={assets.Pic1} />
-              <div className="tooltip">Image 1</div>
+              <img srcSet={assets.pic8} />
+              <div className="tooltip">WORKSHOP</div>
             </div>
             <div className="image-container">
-              <img srcSet={assets.Pic2} />
-              <div className="tooltip">Image 2</div>
+              <img srcSet={assets.pic9} />
+              <div className="tooltip">WORKSHOP</div>
             </div>
             <div className="image-container">
-              <img srcSet={assets.Pic1} />
-              <div className="tooltip">Image 1</div>
+              <img srcSet={assets.pic10} />
+              <div className="tooltip">WORKSHOP</div>
             </div>
             <div className="image-container">
-              <img srcSet={assets.Pic2} />
-              <div className="tooltip">Image 2</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic1} />
-              <div className="tooltip">Image 1</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic2} />
-              <div className="tooltip">Image 2</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic1} />
-              <div className="tooltip">Image 1</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic2} />
-              <div className="tooltip">Image 2</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic1} />
-              <div className="tooltip">Image 1</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic2} />
-              <div className="tooltip">Image 2</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic1} />
-              <div className="tooltip">Image 1</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic2} />
-              <div className="tooltip">Image 2</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic1} />
-              <div className="tooltip">Image 1</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic2} />
-              <div className="tooltip">Image 2</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic1} />
-              <div className="tooltip">Image 1</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic2} />
-              <div className="tooltip">Image 2</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic1} />
-              <div className="tooltip">Image 1</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic2} />
-              <div className="tooltip">Image 2</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic1} />
-              <div className="tooltip">Image 1</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic2} />
-              <div className="tooltip">Image 2</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic1} />
-              <div className="tooltip">Image 1</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic2} />
-              <div className="tooltip">Image 2</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic1} />
-              <div className="tooltip">Image 1</div>
-            </div>
-            <div className="image-container">
-              <img srcSet={assets.Pic2} alt="Image 2" title="Image 2" />
-              <div className="tooltip">Image 2</div>
+              <img srcSet={assets.Pic1} alt="Image 2" title="Image 2" />
+              <div className="tooltip">WORKSHOP</div>
             </div>
           </div>
         </div>
