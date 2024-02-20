cacb183f4a59f01c5d81c7c505c224f628ae87ab
Siddhant Sharma <siddhant.sharma.21cse@bmu.edu.in>
2024-02-20 17:37:31 +0530

    Added lenis smooth scroll

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from "react";
+import React, { useEffect, useLayoutEffect } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
 import About from "../components/About/About";
@@ -8,8 +8,27 @@ import Team from "../components/Team/Team";
 import Events from "../components/Events/Events";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Lenis from "@studio-freight/lenis";
 
 function Homepage() {
+
+  const lenis = new Lenis({
+    duration: 2,
+  })
+
+  lenis.on('scroll', (e) => {
+    console.log(e)
+  })
+  
+  function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+  
+  requestAnimationFrame(raf)
+
+
+
   useLayoutEffect(() => {
     AOS.init();
   }, []);
