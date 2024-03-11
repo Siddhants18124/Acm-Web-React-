cb8d9f8111e501deae383e9db5c310390086fbf4
Siddhant Sharma <siddhant.sharma.21cse@bmu.edu.in>
2024-03-11 21:34:53 +0530

    Added loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,38 @@ import Success from "./components/success/success";
 import Gallerypage from "./pages/Gallerypage";
 import Homepage from "./pages/Homepage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState,useEffect } from "react";
+import {RingLoader} from "react-spinners";
+
 
 function App() {
+
+  const [Loading,setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  } , []);
+
   return (
     <Router>
-      <Routes>
-        <Route element={<Homepage />} path="/" />
-        <Route element={<Gallerypage />} path="/gallery" />
-        <Route element={<Success/>} path="/success" />
-        {/* <Route element={<Error />} path="*" /> */}
-      </Routes>
+      {/* Display the loader when Loading state is true */}
+      {Loading ? (
+        <div className="loader-container" style={{display:"flex",justifyContent: "center", alignItems: 'center', textAlign: "center", width: "100%", height: "100vh"}}>
+          <RingLoader color={"#002EFF"} loading={Loading} size={150}/>
+        </div>
+      ) : (
+        <Routes>
+          <Route element={<Homepage />} path="/" />
+          <Route element={<Gallerypage />} path="/gallery" />
+          <Route element={<Success />} path="/success" />
+        </Routes>
+      )}
     </Router>
   );
 }
+
 
 export default App;
