3a146c239a33d4f7d5f75b3995ab8e62ae420ab8
Siddhant Sharma <siddhant.sharma.21cse@bmu.edu.in>
2024-03-12 16:08:27 +0530

    Fixed the error

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,54 +1,9 @@
 import React from "react";
 import "./Navbar.css";
 import assets from "../../assets/assets";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
-  const [activeLink, setActiveLink] = useState("home");
-  const history = useNavigate();
-
-  useEffect(() => {
-    // Function to handle scrolling to the top when navigating to a new page
-    const handleScrollToTop = () => {
-      window.scrollTo(0, 0);
-    };
-
-    // Add event listener to handle scroll position
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-      const homeSection = document.getElementById("home").offsetTop;
-      const aboutUsSection = document.getElementById("aboutus").offsetTop;
-      const eventsSection = document.getElementById("events").offsetTop;
-      const contactUsSection = document.getElementById("contactus").offsetTop;
-
-      if (scrollPosition >= homeSection && scrollPosition < aboutUsSection) {
-        setActiveLink("home");
-      } else if (
-        scrollPosition >= aboutUsSection &&
-        scrollPosition < eventsSection
-      ) {
-        setActiveLink("aboutus");
-      } else if (
-        scrollPosition >= eventsSection &&
-        scrollPosition < contactUsSection
-      ) {
-        setActiveLink("events");
-      } else {
-        setActiveLink("contactus");
-      }
-    };
-
-    // Add event listener for scroll position
-    window.addEventListener("scroll", handleScroll);
-
-    // Scroll to top when component mounts
-    handleScrollToTop();
-
-    // Remove event listener when component unmounts
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [history.location.pathname]); // Trigger effect when the pathname changes
-
-  // function Navbar() {
   const nav = document.querySelector(".header");
   let lastScrollY = window.scrollY;
   window.addEventListener("scroll", () => {
@@ -82,29 +37,6 @@ function Navbar() {
             </NavLink>
           </li>
           <li>
-            <a
-              href="#aboutus"
-              className={activeLink === "aboutus" ? "active" : ""}
-            >
-              ABOUT&nbsp;US
-            </a>
-          </li>
-          <li>
-            <a
-              href="#events"
-              className={activeLink === "events" ? "active" : ""}
-            >
-              EVENTS
-            </a>
-          </li>
-          <li>
-            <a
-              href="#contactus"
-              className={activeLink === "contactus" ? "active" : ""}
-            >
-              CONTACT&nbsp;US
-            </a>
-
             <a href="#aboutus">ABOUT&nbsp;US</a>
           </li>
           <li>
